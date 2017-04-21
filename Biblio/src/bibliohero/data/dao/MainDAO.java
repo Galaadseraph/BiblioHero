ee081c308e9b733e0689b5cb7b5099984e82ba94
remy <delaforterie.remy@gmail.com>
2017-04-21 16:25:48 +0200

    Connection DAO + Main DAO + ParagrapheDAO

---
+++ Biblio/src/bibliohero/data/dao/MainDAO.java
@@ -0,0 +1,12 @@
+package bibliohero.data.dao;
+import java.sql.*;
+
+public class MainDAO {
+	
+	public static void main(String [] args){
+		
+		ParagrapheDAO test = new ParagrapheDAO();
+		
+		test.recupererTexte();
+  }  
+}
