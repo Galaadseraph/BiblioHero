410c8b035af6bfee93bc52cb109ef1be2895f920
ManWithNoName <deejayone@infonie.fr>
2017-06-05 19:38:46 +0200

    Divers + suppression dao (pour merge ultérieur avec version plus réc)ente

--- src/bibliohero/dao/request/pers_/RestrictionSexeGenreDao.java
+++
@@ -1,7 +0,0 @@
-package bibliohero.dao.request.pers_;
-
-public class RestrictionSexeGenreDao {
-
-		//A remplir???
-		
-}
--- src/bibliohero/service/SrvAdventure.java
+++ src/bibliohero/service/SrvAdventure.java
@@ -1,10 +1,26 @@
 package bibliohero.service;
 
+//import bibliohero.dao.request.pers_.PersonnageDao;
+
 /**
  * Created by jiolle.cdi04 on 01/06/2017.
  */
 public class SrvAdventure {
 
+    // Méthodes de classes statiques appelées par le moteur d'aventure
 
+    /**
+     * Requêtes DAO + Contruction des objets
+     */
+    public static void loadPlayer()
+    {
+        // TODO : Vérifier si la table pers_pjpctempo est POPULE (déjà réalisé lors de la création du person en BDD)
+        // Si c'est le cas, on charge la table pers_pjpctempo et on la stocke dans PJoueurObj
+        // Sinon, on la POPULE dans un premier temps
+
+        // TODO : Requête DAO
+        //PersonnageDao. recupererPersonnageByNom("Nom Personnage")
+
+    }
 
 }
