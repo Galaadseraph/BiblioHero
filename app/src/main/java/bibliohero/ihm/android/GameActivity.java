868af207770bb5e6cf62d3547ec3aa8e68cae9fe
Galaadseraph <galaad.seraphin@gmail.com>
2017-06-08 10:04:12 +0200

    Réparation GameActivity

--- app/src/main/java/bibliohero/ihm/android/GameActivity.java
+++ app/src/main/java/bibliohero/ihm/android/GameActivity.java
@@ -2,12 +2,80 @@ package bibliohero.ihm.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import bibliohero.dao.request.avt_.ActionDao;
+import bibliohero.dao.request.avt_.ParagrapheDao;
+import bibliohero.dao.request.pers_.PersonnageDao;
+import bibliohero.model.avt_.Action;
+import bibliohero.model.avt_.Paragraphe;
+import bibliohero.model.pers_.Personnage;
 
 public class GameActivity extends AppCompatActivity {
+
+    private Personnage joueur;
+    private PersonnageDao persodao;
+
+    private Paragraphe parag;
+    private ParagrapheDao paragdao;
+    private ActionDao adao;
+    private Action[] actions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_main);
+    }
+
+    public void btnPersoClick(View v)
+    {
+        Button b = this.findViewById(R.id.buttonperso);
+        if (b.getText()=="Personnage")
+        {
+            b.setText("Histoire");
+            this.findViewById(R.id.TabPersonnage).setVisibility(View.VISIBLE);
+            this.findViewById(R.id.TabHistoire).setVisibility(View.GONE);
+            this.findViewById(R.id.TabEquipe).setVisibility(View.GONE);
+        }
+        else
+        {
+            b.setText("Personnage");
+            this.findViewById(R.id.TabPersonnage).setVisibility(View.GONE);
+            this.findViewById(R.id.TabHistoire).setVisibility(View.VISIBLE);
+            this.findViewById(R.id.TabEquipe).setVisibility(View.GONE);
+        }
+    }
+
+    public void btnEquipementClick(View v)
+    {
+        Button b = this.findViewById(R.id.buttonObjects);
+        if (b.getText()=="Equipement")
+        {
+            b.setText("Histoire");
+            this.findViewById(R.id.TabPersonnage).setVisibility(View.GONE);
+            this.findViewById(R.id.TabHistoire).setVisibility(View.GONE);
+            this.findViewById(R.id.TabEquipe).setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            b.setText("Equipement");
+            this.findViewById(R.id.TabPersonnage).setVisibility(View.GONE);
+            this.findViewById(R.id.TabHistoire).setVisibility(View.VISIBLE);
+            this.findViewById(R.id.TabEquipe).setVisibility(View.GONE);
+        }
+    }
+
+    //Affiche les infos du personnage dans l'onglet personnage
+    public void initPerso()
+    {
+
+    }
+
+    //Affiche les infos du paragraphe dans l'onglet histoire
+    public void InitAventure()
+    {
+        
     }
 }
